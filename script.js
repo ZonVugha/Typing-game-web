439e0e0e8ab674c4de85185885ec9d8ab18bff8a
Khaki46 <39086187+Khaki46@users.noreply.github.com>
2021-09-08 22:28:54 +0800

    Add  pronounce (US and UK) and adapt to mobile

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let showWordNum = 0;
 let round = 1;
 const val = 30;
 let roundNum = val * round;
+let ukSpeech = "";
+let usSpeech = "";
 
 this.onload = function () {
     const url = "GaoZhongluan_2.json";
@@ -22,9 +24,13 @@ this.onload = function () {
     }
     request.send();
 }
-
 function showWord() {
     remaining.textContent = roundNum - showWordNum;
+    ukSpeech = wordArr[showWordNum].content.word.content.ukspeech;
+    usSpeech = wordArr[showWordNum].content.word.content.usspeech;
+
+    ukPhone.textContent = wordArr[showWordNum].content.word.content.ukphone;
+    usPhone.textContent = wordArr[showWordNum].content.word.content.usphone;
 
     document.querySelector('#headWord').textContent = wordArr[showWordNum].headWord;
     let msg = "";
@@ -53,7 +59,7 @@ const warning = document.querySelector('#warning');
 const inputBox = document.querySelector('#inputBox');
 
 inputWord.addEventListener('keydown', function (e) {
-    if (e.code === 'Enter') {
+    if (e.key === 'Enter') {
         if (inputWord.value == wordArr[showWordNum].headWord) {
             if (roundNum - showWordNum == 1) {
                 btnBox.classList.remove('d-none');
@@ -64,7 +70,7 @@ inputWord.addEventListener('keydown', function (e) {
                 enterIsTrue();
             }
         } else {
-            console.log("you fuck up");
+            console.log("no");
             enterIsFalse();
         }
     }
@@ -85,7 +91,7 @@ function enterIsTrue() {
 // enter is false
 function enterIsFalse() {
     warning.classList.remove('d-none');
-    for (let index = 0; index <+ 6; index++) {
+    for (let index = 0; index < + 6; index++) {
         setTimeout(transform, 100 * index, ((index % 2) * 2 - 1) * 20);
         setTimeout(transform, 100 * 6, 0, 0);
     }
@@ -121,3 +127,32 @@ function next() {
 function transform(x) {
     inputBox.style.transform = `translateX(${x}px)`
 }
+
+const ukPhone = document.querySelector('#ukPhone');
+const usPhone = document.querySelector('#usPhone');
+
+// UK Phone
+ukPhone.addEventListener('click', function () {
+    console.log(ukSpeech);
+
+    const youdaoURL = `https://dict.youdao.com/dictvoice?audio=${ukSpeech}`;
+    const UKAudio = document.querySelector('#UKAudio');
+    const youdaoUKPhone = document.querySelector('#youdaoUKPhone');
+
+    youdaoUKPhone.setAttribute('src', youdaoURL);
+    UKAudio.load();
+    UKAudio.play();
+});
+
+//US phone
+usPhone.addEventListener('click', function () {
+    console.log(usSpeech);
+
+    const youdaoURL = `https://dict.youdao.com/dictvoice?audio=${usSpeech}`;
+    const USAudio = document.querySelector('#USAudio');
+    const youdaoUSPhone = document.querySelector('#youdaoUSPhone');
+    
+    youdaoUSPhone.setAttribute('src', youdaoURL);
+    USAudio.load();
+    USAudio.play();
+});
